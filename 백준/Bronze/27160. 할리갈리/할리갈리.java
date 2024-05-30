1e95e0a4928dcfc941770abd6e7f844c84192147
kubistar <72966727+kubistar@users.noreply.github.com>
2024-05-30 11:53:03 +0900

    [Bronze II] Title: 할리갈리, Time: 496 ms, Memory: 49604 KB -BaekjoonHub

--- 백준/Bronze/27160. 할리갈리/할리갈리.java
+++ 백준/Bronze/27160. 할리갈리/할리갈리.java
@@ -1,21 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         // 펼쳐진 카드의 개수 입력
-        int n = scanner.nextInt();
-        scanner.nextLine(); // 개행 문자 consume
+        int n = Integer.parseInt(br.readLine());
 
         // 각 과일의 종류와 개수를 저장할 Map 생성
         Map<String, Integer> fruitCount = new HashMap<>();
 
         // 카드 정보 읽어들이기
         for (int i = 0; i < n; i++) {
-            String[] cardInfo = scanner.nextLine().split(" ");
+            String[] cardInfo = br.readLine().split(" ");
             String fruit = cardInfo[0];
             int count = Integer.parseInt(cardInfo[1]);
 
@@ -35,6 +36,6 @@ public class Main {
         // 종을 쳐야 하는지 출력
         System.out.println(shouldRing ? "YES" : "NO");
 
-        scanner.close();
+        br.close();
     }
 }
