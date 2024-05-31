6d3abcbfdada2ddd3db1a6e0b17ce7513240293e
kubistar <72966727+kubistar@users.noreply.github.com>
2024-05-31 16:26:00 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 132 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -1,9 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        // BufferedReader를 사용하여 입력을 받기 위한 준비
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 배열의 크기 n을 입력으로 받음
+        int n = Integer.parseInt(br.readLine());
 
         // 코드1의 실행 횟수 계산
         long count = calculateCount(n);
@@ -16,8 +21,10 @@ public class Main {
         System.out.println(degree); // 최고차항의 차수 출력
     }
 
-    // 코드1의 실행 횟수 계산
+    // 코드1의 실행 횟수 계산하는 함수
     public static long calculateCount(int n) {
+        // 코드1의 실행 횟수를 계산하여 반환
+        // 조합의 공식을 사용하여 계산함
         return (long) n * (n - 1) * (n - 2) / 6;
     }
 }
