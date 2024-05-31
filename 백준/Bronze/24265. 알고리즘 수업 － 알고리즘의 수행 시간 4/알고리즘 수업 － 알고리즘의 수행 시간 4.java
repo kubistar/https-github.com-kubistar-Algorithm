bd40fa47483f1f57519f98e755fb9b19dc9db94c
kubistar <72966727+kubistar@users.noreply.github.com>
2024-05-31 15:26:57 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 116 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -1,21 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine()); // 입력을 받음
+
         // 코드1의 수행 횟수 계산
         long count = (long) n * (n - 1) / 2;
-        
-        // 최고차항의 차수 출력
+
+        // 최고차항의 차수 계산
         int degree = 2;
         if (degree > 3) {
             degree = 4;
         }
-        
+
         // 결과 출력
         System.out.println(count);
         System.out.println(degree);
+
+        br.close();
     }
 }
