1a8d565fcd13123f2cf5d252ceab3b909eb82966
kubistar <72966727+kubistar@users.noreply.github.com>
2024-06-05 15:11:37 +0900

    [Bronze II] Title: 그릇, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/7567. 그릇/그릇.java
+++ 백준/Bronze/7567. 그릇/그릇.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         // 괄호 문자열을 입력 받습니다.
-        String brackets = scanner.next();
+        String brackets = br.readLine();
 
         // 그릇의 높이를 계산할 변수를 초기화합니다.
         int height = 10; // 바닥에 놓인 그릇의 높이
@@ -35,6 +37,6 @@ public class Main {
         // 전체의 높이를 출력합니다.
         System.out.println(height);
 
-        scanner.close();
+        br.close();
     }
 }
