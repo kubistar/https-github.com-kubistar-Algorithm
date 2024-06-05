de75d8023c26f49428710f883efdd88c0b8bb62d
kubistar <72966727+kubistar@users.noreply.github.com>
2024-06-05 10:45:05 +0900

    [Bronze II] Title: 막대기, Time: 300 ms, Memory: 26044 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.java
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -1,36 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        // BufferedReader 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         // 막대기의 개수 N 입력 받기
-        int N = scanner.nextInt();
-        
-        // 각 막대기의 높이를 담을 배열 선언
-        int[] heights = new int[N];
-        
+        int N = Integer.parseInt(br.readLine());
+
+        // 각 막대기의 높이를 담을 스택 선언
+        Stack<Integer> stack = new Stack<>();
+
         // 높이 입력 받기
         for (int i = 0; i < N; i++) {
-            heights[i] = scanner.nextInt();
+            stack.push(Integer.parseInt(br.readLine()));
         }
-        
+
         // 현재까지 본 가장 높은 막대기의 높이
         int maxHeight = 0;
         // 보이는 막대기의 개수
         int count = 0;
-        
-        // 오른쪽에서 왼쪽으로 탐색
-        for (int i = N - 1; i >= 0; i--) {
-            if (heights[i] > maxHeight) {
+
+        // 스택을 사용하여 오른쪽에서 왼쪽으로 탐색
+        while (!stack.isEmpty()) {
+            int height = stack.pop();
+            if (height > maxHeight) {
                 count++;
-                maxHeight = heights[i];
+                maxHeight = height;
             }
         }
-        
+
         // 결과 출력
         System.out.println(count);
-        
-        scanner.close();
     }
 }
