1c4f4143c53d40dfd0e54fa4230fc08db080a766
kubistar <72966727+kubistar@users.noreply.github.com>
2024-08-17 21:13:15 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 228 ms, Memory: 19604 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -8,36 +8,50 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         while (true) {
+            // 한 줄의 입력
             String line = br.readLine();
+            
+            // 입력이 "."이면 프로그램 종료
             if (line.equals(".")) {
                 break;
             }
+            
+            // 균형 체크 후 결과 출력
             System.out.println(isBalanced(line));
         }
     }
 
+    // 문자열이 균형을 이루고 있는지 확인하는 메서드
     public static String isBalanced(String s) {
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();  // 괄호를 저장할 스택 생성
 
+        // 문자열의 각 문자를 순차적으로 탐색
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
 
+            // 왼쪽 괄호일 경우 스택에 추가
             if (ch == '(' || ch == '[') {
                 stack.push(ch);
-            } else if (ch == ')') {
+            } 
+            // 오른쪽 소괄호일 경우
+            else if (ch == ')') {
+                // 스택이 비어있거나, 스택의 최상단이 짝이 맞지 않으면 "no" 반환
                 if (stack.isEmpty() || stack.peek() != '(') {
                     return "no";
                 }
-                stack.pop();
-            } else if (ch == ']') {
+                stack.pop();  // 짝이 맞으면 스택에서 제거
+            } 
+            // 오른쪽 대괄호일 경우
+            else if (ch == ']') {
+                // 스택이 비어있거나, 스택의 최상단이 짝이 맞지 않으면 "no" 반환
                 if (stack.isEmpty() || stack.peek() != '[') {
                     return "no";
                 }
-                stack.pop();
+                stack.pop();  // 짝이 맞으면 스택에서 제거
             }
         }
 
-        // 스택이 비어있어야 균형이 잡혀있다고 판단
+        // 문자열을 다 처리한 후, 스택이 비어있으면 "yes" 반환
         return stack.isEmpty() ? "yes" : "no";
     }
 }
