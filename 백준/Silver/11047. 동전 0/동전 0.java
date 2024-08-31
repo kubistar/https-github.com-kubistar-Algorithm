8ada8d3d8eaefaa8b53909db0fffa49348c2dbe9
kubistar <72966727+kubistar@users.noreply.github.com>
2024-08-31 21:23:12 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,16 +1,36 @@
-import java.util.Scanner;
+/*
+입력
+N과 K가 주어진다. (1 ≤ N ≤ 10, 1 ≤ K ≤ 100,000,000)
+ N개의 줄에 동전의 가치 Ai가 오름차순으로 주어진다. 
+ (1 ≤ Ai ≤ 1,000,000, A1 = 1, i ≥ 2인 경우에 Ai는 Ai-1의 배수)
+
+출력
+첫째 줄에 K원을 만드는데 필요한 동전 개수의 최솟값을 출력
+
+K보다 작거나 같은 가장 큰 동전을 찾음
+그 동전으로 가능한 최대한 많이 사용
+그 동전을 사용한 후 남은 금액에 대해 다시 같은 과정을 반복
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // 입력 받기
-        int N = scanner.nextInt();
-        int K = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());      //  동전 가치
+        int K = Integer.parseInt(st.nextToken());      // 동전 합
+        
+        // 동전 가치를 저장할 배열 생성
         int[] coinValues = new int[N];
 
+        // N개의 동전 가치를 입력 
         for (int i = 0; i < N; i++) {
-            coinValues[i] = scanner.nextInt();
+            coinValues[i] = Integer.parseInt(br.readLine());
         }
 
         // 최소 동전 개수 계산 및 출력
